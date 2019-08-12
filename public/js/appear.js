64b8263bd8c1b17e12de15fd724db575c2a2f793
Cvellle <cuekisa@yahoo.com>
2019-08-12 14:13:49 +0200

    a

--- public/js/appear.js
+++ public/js/appear.js
@@ -7,14 +7,34 @@ $(function(){  // $(document).ready shorthand
         
         var bottom_of_element = $(this).offset().top + $(window).height() / 5;
         var bottom_of_window = $(window).scrollTop() + $(window).height();
+        var media = window.matchMedia("(max-width: 905px)");
         
+        if (media.matches) {
+            if( bottom_of_window > bottom_of_element ){
+
+                $(this).addClass("appear");
+                $(this).find('.hideme1').addClass("appear1");
+                $(this).find('.hideme2').addClass("appear2");
+                $(this).find('.hideme3').addClass("appear2");
+                $(this).find('.hideme4').addClass("appear2");
+    
+            }
+    
+        } else {
+
         if( bottom_of_window > bottom_of_element ){
+
             $(this).addClass("appear");
             $(this).find('.hideme1').addClass("appear1");
             $(this).find('.hideme2').addClass("appear2");
             $(this).find('.hideme3').addClass("appear3");
             $(this).find('.hideme4').addClass("appear4");
+
         }
-        
+    }
+
+     
     }); 
 });
+
+
