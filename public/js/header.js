f0c61683f68b105a0d9ee49161c8978494947795
Cvellle <cuekisa@yahoo.com>
2019-08-12 17:01:46 +0200

    a

--- public/js/header.js
+++ public/js/header.js
@@ -55,36 +55,6 @@ if (media.matches) {
   }
 
 
-  
-$(window).scroll(function() {
-  var a = 0;
-  var oTop = $('#counter').offset().top - window.innerHeight;
-  if (a == 0 && $(window).scrollTop() > oTop) {
-    $('.counter-value').each(function() {
-      var $this = $(this),
-        countTo = $this.attr('data-count');
-      $({
-        countNum: $this.text()
-      }).animate({
-          countNum: countTo
-        },
-
-        {
-          duration: 2000,
-          easing: 'swing',
-          step: function() {
-            $this.text(Math.floor(this.countNum));
-          },
-          complete: function() {
-            $this.text(this.countNum);
-          }
-
-        });
-    });
-    a = 1;
-  }
-
-});
 
 
 
