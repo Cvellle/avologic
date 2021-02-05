bb8d7885169b1ca8b4da326158b02ddab3969b2f
cvellle <cuekisa@yahoo.com>
2021-02-05 12:06:46 +0100

    env

--- app.js
+++ app.js
@@ -1,55 +1,59 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var expressHbs = require('express-handlebars');
+var express = require("express");
+var path = require("path");
+var favicon = require("serve-favicon");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var expressHbs = require("express-handlebars");
 var mongoose = require("mongoose");
 var session = require("express-session");
 // connects our back end code with the database
-var routes = require('./routes/index');
+var routes = require("./routes/index");
 var MongoStore = require("connect-mongo")(session);
 var app = express();
 
-mongoose.connect(
-  "mongodb://Cvele:cveledb1@ds129914.mlab.com:29914/posts",
-  { useNewUrlParser: true }
-);
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+
 // view engine setup
-app.engine('.hbs', expressHbs({
-  defaultLayout: 'layout',
-  extname: '.hbs',
-  helpers: {
-    section: function(name, options){
-        if(!this._sections) this._sections = {};
+app.engine(
+  ".hbs",
+  expressHbs({
+    defaultLayout: "layout",
+    extname: ".hbs",
+    helpers: {
+      section: function (name, options) {
+        if (!this._sections) this._sections = {};
         this._sections[name] = options.fn(this);
         return null;
-    }
-}
-}));
-app.set('view engine', '.hbs');
+      },
+    },
+  })
+);
+
+app.set("view engine", ".hbs");
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({
-  secret:'mysupersecret',
-  resave:'false',
-  saveUninitialized:'false',
-  store: new MongoStore({mongooseConnection:mongoose.connection}),
-  cookie: { secure: true }
-  }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: "mysupersecret",
+    resave: "false",
+    saveUninitialized: "false",
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    cookie: { secure: true },
+  })
+);
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', routes);
+app.use("/", routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use(function (req, res, next) {
+  var err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -58,25 +62,24 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+if (app.get("env") === "development") {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render("error", {
       message: err.message,
-      error: err
+      error: err,
     });
   });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.render("error", {
     message: err.message,
-    error: {}
+    error: {},
   });
 });
-
 
 module.exports = app;
